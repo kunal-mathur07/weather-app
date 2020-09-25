9dd6f2019e4886909b3b6923d73d3f34057bf266
Kunal Mathur <kunalmathur@Apples-MacBook-Pro.local>
2020-09-25 18:13:28 +0530

    Updates

---
+++ precache-manifest.3734071c531c490e486b895225ef92e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1414f554186e8e033ca5243c995d2a8a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "94a1bdfe21917b9da4f7",
+    "url": "/weather-app/static/css/main.b28315a0.chunk.css"
+  },
+  {
+    "revision": "c4b8f4d9bcb720ab33e7",
+    "url": "/weather-app/static/js/2.8aa44bce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.8aa44bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a1bdfe21917b9da4f7",
+    "url": "/weather-app/static/js/main.9db5c5bb.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
