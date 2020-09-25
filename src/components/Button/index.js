917032b7a0f2780767dcec739c28d48a8adc8960
Kunal Mathur <kunalmathur@Apples-MacBook-Pro.local>
2020-09-25 21:54:27 +0530

    styling changes and favicon

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,14 +3,10 @@ import './style.css'
 
 class Button extends Component{
 
-    handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            this.props.onClick();
-        }
-      }
+  
     render(){
         return(
-            <div onClick={this.props.onClick} className="btn" onKeyPress={this.handleKeyDown}>
+            <div onClick={this.props.onClick} className="btn" >
                 <div>{this.props.text}</div>
             </div>
         )
